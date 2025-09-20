1310c82d38e0090ffd602cef2cc9068c2f6c5a9d
Emmanuel Musau <musauem98@gmail.com>
2025-09-20 11:00:40 +0300

    py script to solve root-me TCP-Encoded-String challenge"

--- ctfs/root-me-TCP-Encoded-String.py
+++ ctfs/root-me-TCP-Encoded-String.py
@@ -1,12 +1,26 @@
 import socket
+import re
+import base64
+
 with socket.create_connection(("challenge01.root-me.org",52023),timeout=10) as s:
     data = s.recv(1000)
-    print(data.decode(errors="replace"))
-    d = data.decode(errors="replace")
-    print(d.find("my string is"))
+    print(data)
+    
+    pattern = re.compile(r"\s'(.*)'")
+    st = pattern.finditer(str(data))
+    for i in st:
+        print("")
+        enc = i.group().split("'")[1]
+        print(f"encoded string: {enc}")
+    rb_dec = base64.b64decode(enc)
+    dec = str(rb_dec).split("'")[1]
+    print(f"decoded string: {dec}")
+    
+    s.sendall(rb_dec+b"\n")
+    resp = s.recv(1000)
+    print(resp)
 
-    for j in d.splitlines():
-        print(f"> {j}")
-    d.splitlines().
+    
 
-    0702917307
+
+    
